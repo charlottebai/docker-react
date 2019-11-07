fa68562a370bd61c5521d0f48b71315389dc445d
Charlotte Bai <charlotte.bai@everbridge.com>
2019-11-07 12:10:52 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello! I'm changing here. Bye There!  Edit <code>src/App.js</code> and save to reload. ABC
         </p>
         <a
           className="App-link"
